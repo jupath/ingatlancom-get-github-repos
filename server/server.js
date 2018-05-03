114c0900fc6773272baff6f809b6785e2c3d12af
Jupath <attila.taligas@gmail.com>
2018-05-03 22:20:56 +0200

    update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 const publicPath = path.resolve(__dirname, '..', 'public');
-const port = 3000;
+const port = process.env.PORT || 3000; // For Heroku
 
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
